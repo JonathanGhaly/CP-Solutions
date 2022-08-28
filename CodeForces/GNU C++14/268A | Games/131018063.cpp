6be213c12d42f9f4d6294272659cb9607e4f2bef
Jonathan Ghaly <60841107+JonathanGhaly@users.noreply.github.com>
2022-08-28 22:48:22 +0200

    Committing files

---
+++ CodeForces/GNU C++14/268A | Games/131018063.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int n,sol=0;
+    cin>>n;
+    int home[n],away[n];
+    for(int i = 0 ; i < n;++i){
+         cin>>home[i]>>away[i];
+    }
+    for(int i = 0 ; i < n;++i){
+        for(int j = 0 ; j< n;++j){
+            if (home[i]==away[j]){
+                ++sol;
+            }
+ 
+        }
+    }
+    cout<<sol;
+}
